0d08b90806ec04db81ac10fbce0ca076c1459f1f
Jordan Charest <jordancharest3@gmail.com>
2021-08-22 16:04:55 -0400

    Start a nodeJS project; generate a sha-256 hash

---
+++ hashing/app.js
@@ -0,0 +1,21 @@
+var CryptoJS = require("crypto-js");
+
+const data1 = "Blockchain Rock!";
+const dataObject = {
+	id: 1,
+  	body: "With Object Works too",
+  	time: new Date().getTime().toString().slice(0,-3)
+};
+
+/**
+ * Step 3: Add code to the `generate hash function
+ * Function that generate the SHA256 Hash
+ * @param {*} obj 
+ */
+function generateHash(obj) {
+    return CryptoJS.SHA256(JSON.stringify(obj));
+}
+
+console.log(`SHA256 Hash: ${generateHash(data1)}`);
+console.log("************************************");
+console.log(`SHA256 Hash: ${generateHash(dataObject)}`);
